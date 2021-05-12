a164d9c783d0d5570c164ed60bbfdb4a441fc620
akalinoxd <73844729+akalinoxd@users.noreply.github.com>
2021-05-12 08:55:22 -0300

    Add files via upload

---
+++ tabela.js
@@ -0,0 +1,214 @@
+$(function(){
+
+    // Domingo
+    $('#tabD').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // Segunda
+    $('#tabS').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // Terça
+    $('#tabT').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // Quarta
+    $('#tabQ').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // Quinta
+    $('#tabQ2').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // Sexta
+    $('#tabS2').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // Sabado
+    $('#tabS3').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+
+    // 1
+    $('#tab1').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 2
+    $('#tab2').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 3
+    $('#tab3').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 4
+    $('#tab4').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 5
+    $('#tab5').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 6
+    $('#tab6').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 7
+    $('#tab7').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 8
+    $('#tab8').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 9
+    $('#tab9').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 10
+    $('#tab10').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 11
+    $('#tab11').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 12
+    $('#tab12').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 13
+    $('#tab13').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 14
+    $('#tab14').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 15
+    $('#tab15').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 16
+    $('#tab16').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 17
+    $('#tab17').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 18
+    $('#tab18').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 19
+    $('#tab19').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 20
+    $('#tab20').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 21
+    $('#tab21').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 22
+    $('#tab22').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 23
+    $('#tab23').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 24
+    $('#tab24').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 25
+    $('#tab25').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 26
+    $('#tab26').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 27
+    $('#tab27').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 28
+    $('#tab28').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 29
+    $('#tab29').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 30
+    $('#tab30').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 31
+    $('#tab31').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 32
+    $('#tab32').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 33
+    $('#tab33').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 34
+    $('#tab34').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+    // 35
+    $('#tab35').click(function(){
+        $(this).css('background', 'yellow')
+    })
+
+})
