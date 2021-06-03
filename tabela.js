d3d9b89ad0abf280200fc935a4562ab78284e167
akalinoxd <73844729+akalinoxd@users.noreply.github.com>
2021-06-03 09:26:36 -0300

    Update tabela.js

--- tabela.js
+++ tabela.js
@@ -2,213 +2,241 @@ $(function(){
 
     // Domingo
     $('#tabD').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // Segunda
     $('#tabS').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // Terça
     $('#tabT').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // Quarta
     $('#tabQ').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // Quinta
     $('#tabQ2').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // Sexta
     $('#tabS2').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // Sabado
     $('#tabS3').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
 
     // 1
     $('#tab1').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 2
     $('#tab2').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 3
     $('#tab3').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 4
     $('#tab4').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 5
     $('#tab5').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 6
     $('#tab6').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 7
     $('#tab7').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 8
     $('#tab8').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 9
     $('#tab9').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 10
     $('#tab10').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 11
     $('#tab11').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 12
     $('#tab12').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 13
     $('#tab13').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 14
     $('#tab14').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 15
     $('#tab15').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 16
     $('#tab16').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 17
     $('#tab17').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 18
     $('#tab18').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 19
     $('#tab19').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 20
     $('#tab20').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 21
     $('#tab21').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 22
     $('#tab22').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 23
     $('#tab23').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 24
     $('#tab24').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 25
     $('#tab25').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 26
     $('#tab26').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 27
     $('#tab27').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 28
     $('#tab28').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 29
     $('#tab29').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 30
     $('#tab30').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 31
     $('#tab31').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 32
     $('#tab32').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 33
     $('#tab33').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 34
     $('#tab34').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
     })
 
     // 35
     $('#tab35').click(function(){
-        $(this).css('background', 'yellow')
+        $(this).css('background', 'red')
+    })
+    //36
+    $('#tab36').click(function(){
+        $(this).css('background', 'red')
+    })
+    //37
+    $('#tab37').click(function(){
+        $(this).css('background', 'red')
+    })
+    //38
+    $('#tab38').click(function(){
+        $(this).css('background', 'red')
+    })
+    //39
+    $('#tab39').click(function(){
+        $(this).css('background', 'red')
+    })
+    //40
+    $('#tab40').click(function(){
+        $(this).css('background', 'red')
+    })
+    //41
+    $('#tab41').click(function(){
+        $(this).css('background', 'red')
+    })
+    //42
+    $('#tab42').click(function(){
+        $(this).css('background', 'red')
     })
 
 })
